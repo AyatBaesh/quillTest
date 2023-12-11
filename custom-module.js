ad42e9f7b80a4817a31500c1e295a4a4dfcc01dc
Ayat <ayatbaeshev@gmail.com>
2023-12-12 00:22:34 +0100

    created a custom module, re-writed functionality. Working on Delta representation

--- custom-module.js
+++ custom-module.js
@@ -1,17 +1,14 @@
 //Quill functionality
-var quill = new Quill('#editor', {
-    theme: 'snow',
-    modules: {
-    toolbar: '#toolbar'
-    }
-});
-
 var Block = Quill.import('blots/block');
 
 class QImageBlot extends Block {
     static create(value) {
         let node = super.create();
-        node.innerHTML = `<img src="${value.image}"/>\n${value.text}`;
+        node.innerHTML = `<img src="${value.image}"/>${value.text}`;
+        // node.innerHTML = `<figure>
+        //                     <img src=${value.image} />
+        //                     <figcaption>${value.text}</figcaption>
+        //                  </figure>`
         return node;
     }
 
@@ -27,81 +24,156 @@ QImageBlot.blotName = 'qImage';
 QImageBlot.tagName = 'q-img-text';
 Quill.register(QImageBlot);
 
+class CustomBar {
 
-const form = document.querySelector('#customForm');
-const imageFile = document.querySelector('#imageFile');
-const imageURL = document.querySelector('#url');
-const inputText = document.querySelector('#text'); 
-const inputButton = document.querySelector('#inputButton');
-const toolbar = quill.getModule('toolbar');
-const button = document.createElement('button');
-const editor = document.querySelector('#editor');
-
-editor.addEventListener('dragover', (e) => {e.preventDefault()});
-editor.addEventListener('drop', (e) => {
-    e.preventDefault();
-    const file = e.dataTransfer.files[0];
-    const reader =  new FileReader();
-    reader.onload = () => {
-        // const convertedImg = e.target.result;
-        document.querySelector('#imageFile').files = e.dataTransfer.files;
-        form.style.display = 'block';
+    constructor(quill, options) {
+        this.quill = quill;
+        this.options = options;
+        this.initToolbarButton();
+        this.form = this.createForm();
+        this.initDragAndDrop();
     }
-    reader.readAsDataURL(file);
-    
+
+    insertContent() {
+        const urlInput = document.querySelector('#url');
+        const imageInput = document.querySelector('#imageFile');
+        const textInput = document.querySelector('#textInput')
+        const inputButton = document.querySelector('#inputButton');
+        let image = '';
+        let text = '';
+        inputButton.addEventListener('click',() => {
+            text = textInput.value;
+            if(urlInput.disabled){
+                image = this._convertToBase64(imageInput.files[0], (convertedImage) => {
+                    this.insertQimageText(convertedImage, text);
+                });
+            }else if(imageInput.disabled){
+                image = urlInput.value;
+                this.insertQimageText(image, text);
+            }
+        document.querySelectorAll('input').forEach(inp => {inp.value = ''});
+        this.form.style.display = 'none';
+        
+        })
+    }
+
+    initToolbarButton() {
+        const button = document.querySelector('#qImage');
+        button.addEventListener('click',() => {
+            console.log('has worked')
+            this.form.style.display = 'block';
+            this.insertContent();
+        })
+    }
+
+    insertQimageText(imageSrc, imageText) {
+        this.quill.insertEmbed(
+            this.quill.getSelection('true').index, 
+            'qImage',
+            {image: imageSrc, text: imageText}
+        );
+    }
+
+    createForm() {
+        const formDiv = document.createElement('div');
+        formDiv.id = 'customForm';
+        formDiv.style.display = 'none';
+
+        const textInput = document.createElement('input');
+        textInput.type = 'text';
+        textInput.id = 'textInput';
+        textInput.placeholder = 'Enter Image Text';
+
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.id = 'imageFile';
+        fileInput.accept = 'image/*';
+        
+        const urlInput = document.createElement('input');
+        urlInput.type = 'text';
+        urlInput.id = 'url';
+        urlInput.placeholder = 'Enter Image URL';
+
+        urlInput.addEventListener('input', handleInputChange);
+        fileInput.addEventListener('input', handleInputChange);
+
+        function handleInputChange() {
+        if (urlInput.value) {
+            fileInput.disabled = true;
+        } else if (fileInput.value) {
+            urlInput.disabled = true;
+        } else {
+            urlInput.disabled = false;
+            fileInput.disabled = false;
+        }
+        }
+
+        const addButton = document.createElement('button');
+        addButton.id = 'inputButton';
+        addButton.textContent = 'Add';
+
+        formDiv.appendChild(textInput);
+        formDiv.appendChild(fileInput);
+        formDiv.appendChild(urlInput);
+        formDiv.appendChild(addButton);
+        document.body.appendChild(formDiv);
+        return formDiv;
+
+    }
+
+    initDragAndDrop() {
+        const editor = this.quill.container;
+        editor.addEventListener('dragover', (e) => {e.preventDefault()});
+        editor.addEventListener('drop', (e) => {
+            e.preventDefault();
+            const file = e.dataTransfer.files[0];
+            this._convertToBase64(file, (convertedImage) => {
+                let text = prompt('Enter image text'); //FIXME Add text input and read value;
+                const image = convertedImage;
+                this.insertQimageText(image, text);
+            }); 
+        })
+    }
+
+    _convertToBase64(file, callback) {
+        if(file){
+            const reader = new FileReader();
+            reader.onload = () => callback(reader.result);
+            reader.readAsDataURL(file);
+        }else{
+            console.error('No file');
+        }
+    }
+
+} 
+
+Quill.register('modules/custombar', CustomBar);
+
+var quill = new Quill('#editor', {
+    theme: 'snow',
+    modules: {
+        toolbar: '#toolbar',
+        custombar: {},
+    }
 
 });
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-});
 
-button.innerHTML = 'Add Image&Text';
-button.onclick = () => {
-    form.style.display = 'block';
+const Delta = Quill.import('delta');
+quill.updateContents(new Delta().retain(10).insert({
+  qImage: {
+    image: 'https://picsum.photos/200/300',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  }
+}));
+
+
+function convertDeltaToJSON() {
+    const delta = quill.getContents();
+    const deltaJSON = delta.ops;
+    return JSON.stringify(deltaJSON, null, 2);
 }
 
-toolbar.addHandler('qImage', (file = '', text = '', imageLink = '') => {
-    if(file){
-        console.log(`file is ${file}`)
-        let reader = new FileReader();
-        reader.onload = (e) => {
-            const convertedImg = e.target.result;
-            quill.insertEmbed(quill.getSelection(true).index + 1, 'qImage', { image: convertedImg, text: text });
-        }
-        reader.readAsDataURL(file);
-    }else if(imageLink){
-        quill.insertEmbed(quill.getSelection(true).index + 1, 'qImage', { image: imageLink, text: text });
-    }
 
-  
-});
-inputButton.onclick = () => {
-    if(imageFile.files[0] && inputText.value){
-        imageURL.value = '';
-        toolbar.handlers.qImage(file = imageFile.files[0], text = inputText.value, imageLink = '');
-        form.style.display = 'none';
-        document.querySelectorAll('input').forEach(input => {
-            input.value = '';
-        });
-    }else if(imageURL && inputText.value){
-        imageFile.value = '';
-        toolbar.handlers.qImage(file = '', text = inputText.value, imageLink = imageURL.value);
-        form.style.display = 'none';
-        document.querySelectorAll('input').forEach(input => {
-            input.value = '';
-        });
-    }
-};
-document.getElementById('toolbar').appendChild(button);
-
-
-// var delta = [{
-//     "insert": {
-//         "qImage": {
-//             "image": "https://picsum.photos/200/300",
-//             "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-//         }
-//     }
-// }];
-// editor.updateContents(delta)
+const deltaRepresentationJSON = convertDeltaToJSON();
+console.log(deltaRepresentationJSON);
